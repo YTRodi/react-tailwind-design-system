2519531a158abc3319c9b4187c25ca7adc12a1df
YTRodi <yagorodi@gmail.com>
2022-11-06 11:15:26 -0300

    feat: set colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,32 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      current: colors.current,
+      transparent: colors.transparent,
+      white: colors.white,
+      black: colors.black,
+      neutral: colors.slate,
+      brand: colors.indigo,
+      rose: {
+        600: colors.rose['600'],
+        400: colors.rose['400'],
+        50: colors.rose['50'],
+      },
+      amber: {
+        600: colors.amber['600'],
+        400: colors.amber['400'],
+        50: colors.amber['50'],
+      },
+      teal: {
+        600: colors.teal['600'],
+        400: colors.teal['400'],
+        50: colors.teal['50'],
+      },
+    },
     extend: {},
   },
   plugins: [],
