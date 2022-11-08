b8e6781805b3e92a95c65da1e01e27b4fb2f668e
YTRodi <yagorodi@gmail.com>
2022-11-08 20:25:30 -0300

    feat: app and tooltip provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Cog6ToothIcon } from '@heroicons/react/20/solid';
 
 import { Button, TextButton, IconButton } from './components';
+import AppProviders from './context';
 
 function App() {
   return (
-    <div>
-      <Button
-        disabled
-        loading
-        label="Click me!"
-        leftIcon={<Cog6ToothIcon />}
-        rightIcon={<Cog6ToothIcon />}
-      />
-      <IconButton icon={<Cog6ToothIcon />} />
-      <TextButton disabled label="HolaMundo" />
-    </div>
+    <AppProviders>
+      <div>
+        <Button
+          disabled
+          loading
+          label="Click me!"
+          leftIcon={<Cog6ToothIcon />}
+          rightIcon={<Cog6ToothIcon />}
+        />
+        <IconButton icon={<Cog6ToothIcon />} />
+        <TextButton disabled label="HolaMundo" />
+      </div>
+    </AppProviders>
   );
 }
 
---
+++ src/components/atoms/Tooltip/index.tsx
@@ -0,0 +1,6 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+
+export const TooltipProvider = TooltipPrimitive.Provider;
+export const Tooltip = TooltipPrimitive.Root;
+export const TooltipTrigger = TooltipPrimitive.Trigger;
+export const TooltipContent = TooltipPrimitive.Content;
